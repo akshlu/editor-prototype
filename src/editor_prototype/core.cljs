49d91031a9ebe14e0b267f620ecdf89d97fb70e1
Alexey Ustinov <alvustinov@gmail.com>
2018-05-16 00:36:48 +0300

    updated styles

--- src/editor_prototype/core.cljs
+++ src/editor_prototype/core.cljs
@@ -7,13 +7,13 @@
 (enable-console-print!)
 
 
-(defonce app-state (reagent/atom {:text "Hello world!"}))
+(defonce app-state (reagent/atom {:text "Editor Prototype"}))
 
 
 (defn hello-world []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h2 (:text @app-state)]
+   [:h4 "Edit this and watch it change!"]])
 
 
 (reagent/render-component [hello-world]
